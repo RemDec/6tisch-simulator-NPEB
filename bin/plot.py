e49aa21464f02d1395a6ce4b51d11216358cecec
remdec <remydecocq@gmail.com>
2020-06-06 12:57:06 +0200

    compute stats over join steps and writing in a file

--- bin/plot.py
+++ bin/plot.py
@@ -132,6 +132,13 @@ def str_global_stats(global_stats):
     return "Number of nodes : %d   Number of runs : %d" % (global_stats['nbr_motes'], global_stats['nbr_runs'])
 
 
+def write_additional_stats(output_folder, s, filename="add_stats.txt"):
+    if not os.path.isdir(output_folder):
+        os.makedirs(output_folder)
+    with open(output_folder+'/'+filename, 'a+') as f:
+        f.write(s)
+
+
 def plot_phase_times_boxplots(phase_times, global_stats, subfolder):
     conv_times = global_stats['convergence_times']
     mean_conv_time = float(sum(conv_times)) / len(conv_times)
@@ -145,6 +152,10 @@ def plot_phase_times_boxplots(phase_times, global_stats, subfolder):
         data['value'].extend(vals)
 
     df = pd.DataFrame(data)
+    stats_join_steps = df.groupby('phase').describe().to_string()
+    stats_write = "--- Time elapsed at steps of join process - " + str_global_stats(global_stats) + '\n' +\
+                  stats_join_steps + '\n\n'
+    write_additional_stats(subfolder, stats_write)
     ax = sns.boxplot(y='phase', x='value', data=data, orient='h', linewidth=1,
                      order=[labels[t] for t in stat_names[::-1]],
                      palette=['OrangeRed', 'Orange', 'Gold'],
@@ -171,6 +182,10 @@ def plot_phase_charges_boxplots(phase_charges, global_stats, subfolder):
         data['value'].extend(map(lambda microC: microC/1000, vals))
 
     df = pd.DataFrame(data)
+    stats_join_steps = df.groupby('phase').describe().to_string()
+    stats_write = "--- Charge consumed at steps of join process - " + str_global_stats(global_stats) + '\n' +\
+                  stats_join_steps + '\n\n'
+    write_additional_stats(subfolder, stats_write)
     ax = sns.boxplot(y='phase', x='value', data=data, orient='h', linewidth=1,
                      palette=['Tomato', 'OrangeRed', 'Orange', 'Gold'],
                      order=[labels[t] for t in stat_names[::-1]],
